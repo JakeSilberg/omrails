0d8d46c8ec86899b07b2e775172bfeb82c8e08de
Jake Silberg <JakeSilberg@gmail.com>
2013-07-13 15:19:59 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = '3e4705d1ea03ae9ad405ec4f57951207df0a88d10d555a36a184f3a98271bc3b3384c5b175cdc735c66cba1ef87d5eabf0516f621494702585ede24d9023a9da'
